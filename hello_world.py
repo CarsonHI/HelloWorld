b780b462fdf33123e6777c979916f6fbd942b4af
CarsonHI <carson.healy@tis.edu.mo>
2024-08-07 20:14:34 +0800

    added input function for food

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,7 @@
 print("Hello World!")
+print("What food do you like?")
+x = input()
+if x.lower() == "pasta":
+    print("HI PASTA LOVER!")
+else:
+    print("NOOOOO WRONG ANSWER!!! DID YOU MEAN PASTA?")
