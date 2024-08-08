62d7e641d522121adc8b351ff6fa34809bde8212
CarsonHI <carson.healy@tis.edu.mo>
2024-08-08 20:30:37 +0800

    added stuff

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,10 @@
-#print("Hello World!")
-#print("What food do you like?")
-#x = input()
-#if x.lower() == "pasta":
-  #  print("HI PASTA LOVER!")
-#else:
- #   print("NOOOOO WRONG ANSWER!!! DID YOU MEAN PASTA?")
+print("Hello")
+print("What food do you like?")
+x = input()
+if x.lower() == "pasta":
+    print("HI PASTA LOVER!")
+else:
+    print("NOOOOO WRONG ANSWER!!! DID YOU MEAN PASTA?")
 
 percent = int(input("Enter quiz score (out of 100): "))
 
