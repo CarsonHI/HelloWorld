d79043418c6bfef9e3a308ce1c6a6d72cf44b191
CarsonHI <carson.healy@tis.edu.mo>
2024-08-08 20:26:44 +0800

    Added telling mom and dad.

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,23 @@
-print("Hello World!")
-print("What food do you like?")
-x = input()
-if x.lower() == "pasta":
-    print("HI PASTA LOVER!")
+#print("Hello World!")
+#print("What food do you like?")
+#x = input()
+#if x.lower() == "pasta":
+  #  print("HI PASTA LOVER!")
+#else:
+ #   print("NOOOOO WRONG ANSWER!!! DID YOU MEAN PASTA?")
+
+percent = int(input("Enter quiz score (out of 100): "))
+
+if percent == 100:
+  print("Here Is 30$ !")
+  print("Telling Mom And Dad..............")
+elif percent >= 90:
+  print("Here Is your 20$")
+  print("Telling Mom And Dad..............")
+elif percent >= 75:
+  print("Here Is your 10$")
+  print("Telling Mom And Dad..............")
 else:
-    print("NOOOOO WRONG ANSWER!!! DID YOU MEAN PASTA?")
+  print("Great effort!")
+  print ("Telling Mom And Dad..............")
+percent = int(input("Enter quiz score (out of 100): "))
