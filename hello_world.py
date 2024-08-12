dc3bbbd6eb73e6d823e1bd786a502c16b7df9b82
CarsonHI <carson.healy@tis.edu.mo>
2024-08-13 01:42:48 +0800

    Debuged, added dots

--- hello_world.py
+++ hello_world.py
@@ -6,27 +6,32 @@ if x.lower() == "pasta":
 else:
     print("NOOOOO WRONG ANSWER!!! DID YOU MEAN PASTA?")
 
-percent = int(input("Enter quiz score (out of 100): "))
+percent = int(input("Enter quiz score  "))
 
 if percent == 100:
   print("Here Is 30$ !")
   print("Telling Mom And Dad..............")
 elif percent >= 90:
-  print("Here Is your 20$")
+  print("Here Is your 20$.")
   print("Telling Mom And Dad..............")
 elif percent >= 75:
-  print("Here Is your 10$")
+  print("Here Is your 10$.")
   print("Telling Mom And Dad..............")
 else:
   print("Great effort!")
   print ("Telling Mom And Dad..............")
+  print ("Add teacher note")
 
 print ("Did the teacher keep you in for recces?")
 k = input()
 
 if k.lower() == "no":
-    print("Yeah, good job. Telling Mom and Dad")
-elif k.lower() == "yes":
+    print("Yeah, good job. Telling Mom and Dad.")
+
+elif k.lower() == "yes, sorry. ":
+   
+else :
     print("Not good, Telling Mom and Dad........")
     print("Add teacher note.")
-   
+    
+print("Ok here is your homework.")  
