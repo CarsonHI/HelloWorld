b5d438553fd7e79370ae5bf6e5394c89d3559179
CarsonHI <carson.healy@tis.edu.mo>
2024-08-15 16:02:32 +0800

    Added racamedation tool

--- hello_world.py
+++ hello_world.py
@@ -30,8 +30,32 @@ if k.lower() == "no":
 
 elif k.lower() == "yes, sorry. ":
    
+  print("Its alright.")
+   
 else :
     print("Not good, Telling Mom and Dad........")
     print("Add teacher note.")
     
 print("Ok here is your homework.")  
+
+fin_lit = "Financial Literacy"
+pixar = "Pixar in a Box"
+grammar = "Grammar"
+
+# Collect user attributes to inform our recommendation.
+grade = int(input("What grade are you in? "))
+favorite_subject = input("What is your favorite subject? ")
+
+# Make a course recommendation based on the user's attributes.
+rec = ""
+if grade < 6:
+    # Course caters to younger learners.
+    rec = grammar
+else:
+    if favorite_subject == "computing" or favorite_subject == "art":
+        rec = pixar
+    else:
+        # Most broadly relevant and approachable subject.
+        rec = fin_lit
+
+print("We recommend this course: " + rec)
